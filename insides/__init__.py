4bf0ab29d131b6a42e9ec4f690f0be3b9d7f8139
Rapidsafeguard <rapidsafeguard@gmail.com>
2019-09-04 13:31:43 +0530

    1.0

---
+++ insides/__init__.py
@@ -0,0 +1,8 @@
+"""
+
+"""
+
+from colors 	import *
+from functions 	import *
+from banner 	import banner as Banner
+from footer 	import footer as Footer
---
+++ insides/footer.py
@@ -0,0 +1,15 @@
+
+from colors import *
+
+footer = """
+  {white}[{green}${white}] {cyan}Thanks For Using {white}:D
+\t{blue}~ {red}www.easyhack.in {white}:)
+""".format(
+        white = w,
+        green = g,
+        cyan = c,
+        blue = b,
+        red = r
+    )
+
+# print footer
